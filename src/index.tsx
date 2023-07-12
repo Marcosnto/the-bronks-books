9445ad640dd3283c837001da7871b9d6785ee5a5
Marcos Neto <marcos_nto@hotmail.com>
2023-07-11 22:43:55 -0300

    build: add react router dom and update readme
    - add router on App component
    - remove default orietations from ReadMe

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,7 +12,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />  
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
