b8a22c21d61ec4cc5572d32bb9f2e6e65b2c1b79
Marcos Neto <marcos_nto@hotmail.com>
2023-07-12 16:19:33 -0300

    test: remove test for home page

--- src/pages/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "./Home";
-
-test("renders hello world react link", () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/hello world/i);
-  expect(linkElement).toBeInTheDocument();
-});
