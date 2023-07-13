6e27a8338534233e4796a176d5f1beca895a7c0d
Marcos Neto <marcos_nto@hotmail.com>
2023-07-13 13:35:36 -0300

    feat: put FetchBookDetails on Details page

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,3 +1,28 @@
+import { useQuery } from "react-query";
+import fetchBookDetails from "../api/fetchBookDetails";
+import Header from "../components/Header";
+import { useParams } from "react-router-dom";
+
 export default function Details() {
-  return <h1>Details</h1>;
+  const { bookID } = useParams();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["book", bookID],
+    queryFn: () => fetchBookDetails(bookID),
+  });
+
+  if (isLoading) {
+    return <h1>carregando...</h1>;
+  }
+
+  if (isError) {
+    return <h1>erro</h1>;
+  }
+  console.log(data);
+  return (
+    <>
+      <Header title="BookStore" />
+      <h1>Details {bookID}</h1>
+    </>
+  );
 }
