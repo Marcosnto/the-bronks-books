0cb249f7c1901cea3b5f7c37b761d3a040223270
Marcos Neto <marcos_nto@hotmail.com>
2023-07-12 23:26:42 -0300

    style: add margin bottom to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,6 +9,7 @@ export default function Header({ title }: HeaderProps) {
         text-center 
         text-4xl  
         font-bold
+        mb-4
         text-slate-50
         bg-emerald-600 
         p-5"
