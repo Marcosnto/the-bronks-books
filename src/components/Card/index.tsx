c102dfb55d670482f8b59267e783c2b2e9d6206a
Marcos Neto <marcos_nto@hotmail.com>
2023-07-13 00:41:37 -0300

    style: modify styles from card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -16,26 +16,36 @@ export default function Card({
       key={id}
       className="
     bg-slate-100 
-      w-40 flex 
+      w-60
+      max-w-sm
+      flex 
       flex-col 
       gap-3
       p-3 
       min-w-fit
-      rounded-lg"
+      rounded-lg
+      items-center
+      "
     >
       <Link to="/">
-        <img src={imageLink} alt={`Capa do livro ${title}`} className="w-40" />
+        <img
+          src={imageLink}
+          alt={`Capa do livro ${title}`}
+          className="w-40 h-40 "
+        />
       </Link>
       <div className="flex flex-col gap-1">
         <Link to="/">
-          <header className="text-md font-light">{title}</header>
+          <header className="text-md font-light flex-wrap">{title}</header>
         </Link>
         <span className="text-sm text-gray-500">
           {author} - {year}
         </span>
       </div>
       <span className="font-bold text-gray-950">R$ {price}</span>
-      <Button label="Comprar" onClick={() => console.log("comprei")} />
+      <div className="w-full">
+        <Button label="Comprar" onClick={() => console.log("comprei")} />
+      </div>
     </div>
   );
 }
