6c061cc622f280e4379ea6e4d0dec1a4eed254d3
Marcos Neto <marcos_nto@hotmail.com>
2023-07-14 01:44:09 -0300

    chore: remove unecessary react call

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import Home from "./pages/Home";
-
-import "./index.css";
 import Details from "./pages/Details";
 import Cart from "./pages/Cart";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+import "./index.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
