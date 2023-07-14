4a8692597c07dd53ba9d89f1f5ad3bfec59278a9
Marcos Neto <marcos_nto@hotmail.com>
2023-07-13 21:28:20 -0300

    fix(test): put BrowserRouter on Header test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Header from ".";
 
+import { BrowserRouter } from "react-router-dom";
+
 describe("header", () => {
   test("should render with correct name", () => {
-    render(<Header title="ShopTest" />);
+    render(
+      <BrowserRouter>
+        <Header title="ShopTest" />
+      </BrowserRouter>,
+    );
 
     const header = screen.getByText(/ShopTest/i);
 
